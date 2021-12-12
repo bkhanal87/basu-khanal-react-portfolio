930705036a8e1882d9de530243db39594588ca13
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-12-11 22:24:25 -0500

    react app created

---
+++ basu-khanal-react-portfolio/src/App.js
@@ -0,0 +1,44 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// src/App.js
+
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Project from "./components/Project";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Footer />
+      <Header />
+      <Navbar />
+      <Project />
+    </main>
+  );
+}
