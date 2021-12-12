7b707553c0754a70e31d187b8ecc0f9796d4e6e4
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-12-12 14:02:12 -0500

    header created

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,13 @@
+import "./contact.scss";
+
+
+
+
+export default function Contact() {
+    return (
+      <div className="contact">
+        {/* name, an email address, and a message form (notification that this field is required*/}
+      </div>
+    );
+  }
+  
