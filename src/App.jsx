130c323a645edb4948696e9e22f4bc274572385b
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-12-12 14:43:26 -0500

    snap-scroll effect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import Header from "./components/header/Header";
 
+import About from "./components/about/About";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import Resume from "./components/resume/Resume";
+
+import "./app.scss"
+
+
+
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className="sections">
+        <About />
+        <Portfolio />
+        <Contact />
+        <Resume />
+      </div>
     </div>
   );
 }
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,6 +7,7 @@ export default function Contact() {
     return (
       <div className="contact">
         {/* name, an email address, and a message form (notification that this field is required*/}
+        my phone number is 
       </div>
     );
   }
