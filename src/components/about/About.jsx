438e71adf5656511c339b9af6ee0a8fe97714188
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-12-15 20:21:50 -0500

    intro section created

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,20 @@
-import './about.scss';
+import "./about.scss";
 
 export default function About() {
-    return (
-        <div className="about" id="about">
+  return (
+    <div className="about" id="about">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="asset/basu.jpg" alt="Basu Khanal" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Basu Khanal</h1>
+          <h3>Software Developer</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
