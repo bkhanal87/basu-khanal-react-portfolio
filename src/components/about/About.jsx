a6faddfb6cb957886ec8324b539198ad90a9f0e2
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-12-19 01:40:35 -0500

    saving latest updates

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,8 +12,13 @@ export default function About() {
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Basu Khanal</h1>
-          <h3>Software Developer</h3>
+          <h3>
+            Software <span>Developer</span>
+          </h3>
         </div>
+        <a href="#portfolio">
+          <img src="asset/down-arrow.png" alt="A down arrow" />
+        </a>
       </div>
     </div>
   );
