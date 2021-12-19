1c26b8f012f08f02bd9a0342e761f65c095b4b56
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-12-18 19:57:24 -0500

    contact page created

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,30 @@
+import { SpaRounded } from "@material-ui/icons";
+import { useState } from "react";
 import "./contact.scss";
 
-
-
-
 export default function Contact() {
-    return (
-      <div className="contact" id="contact">
-        {/* name, an email address, and a message form (notification that this field is required*/}
-        my phone number is 
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="left"></div>
+
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP</span>}
+        </form>
       </div>
-    );
-  }
-  
+
+      {/* name, an email address, and a message form (notification that this field is required*/}
+    </div>
+  );
+}
