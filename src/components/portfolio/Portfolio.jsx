52d53b7135f2e3353e8685ce9fe83ea3e5b7f213
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-12-31 12:09:24 -0500

    images of projects added to asset folder

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,7 @@ import "./portfolio.scss";
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      the world is full of happiness mmmmmmmmmmmmmmmmmmm
+      <h1>Projects</h1>
     </div>
   );
 }
